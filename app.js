62aee24d3125372db6483456aaf486fee7c1d9dc
NECKERRMAN <robin.deneckere@gmail.com>
2022-10-29 15:40:44 +0200

    refactor: Simplify code

--- app.js
+++ app.js
@@ -1,37 +1,32 @@
 import { discover, SCENES } from "wikari";
 
-// Toggle Bulb
-const toggleBulb = async () => {
-         
+const initBuld = async () => {
+    // Find bulb on network
     const bulbs = await discover({ addr: process.env.WIZ_ADDR });
-    
     const bulb = bulbs[0];
+
+    // Return if no bulb found
     if (!bulb) return;
 
-    const details = await bulb.getPilot()
-
     // whenever the bulb sends a message, log it to the console
-    // bulb.onMessage(console.log);
-    console?.log(details);
+    const details = await bulb.getPilot()
+    console.log(details);
 
+    return bulb;
+};
+
+// Toggle Bulb
+const toggleBulb = async () => {
+    // Init bulb     
+    const bulb = await initBuld();
     // turn the bulb on
     await bulb.toggle();
 } 
 
 // Set bulb  state
 const setBulb = async (state) => {
-       
-    const bulbs = await discover({ addr: process.env.WIZ_ADDR});
-    
-    const bulb = bulbs[0];
-    if (!bulb) return;
-
-    const details = await bulb.getPilot()
-
-    // whenever the bulb sends a message, log it to the console
-    // bulb.onMessage(console.log);
-    console.log(details);
-
+    // Init bulb   
+    const bulb = await initBuld();
     // turn the bulb on
     await bulb.turn(state);
 } 
